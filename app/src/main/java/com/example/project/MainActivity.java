c59147c3b5b86ce916233d1e31688b2226e22533
elyyz <a23eliax@student.his.se>
2024-05-09 15:44:28 +0200

    Tog bort second activity och lade till en webview som heter "Om oss"

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -15,12 +22,26 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private ArrayList<Spel> spel= new ArrayList<>();
     private ArrayList<RecyclerViewItem> recyclerViewItems=new ArrayList<>();
     private RecyclerViewAdapter adapter;
+
+    public void showInternalWebPage(){
+        // TODO: Add your code for showing internal web page here
+        WebView webView = findViewById(R.id.My_WebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/about.html");
+    }
+
+    WebView myWebView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myWebView = findViewById(R.id.My_WebView);
+        myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
 
         spel.add(new Spel("1","a23eliax", "Catan", "Brädspel", 399));
         spel.add(new Spel("2","a23eliax", "Ticket to Ride", "Brädspel", 299));
@@ -41,6 +62,25 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         });
 
         new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+       int id = item.getItemId();
+        if (id == R.id.action_internal_web) {
+            Log.d("hej","Will display internal web page");
+
+            showInternalWebPage();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
